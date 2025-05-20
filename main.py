1e547ab466b8320310e6b040eda98c94aaa9964e
Ivo Rocha <ivocunha077@gmail.com>
2025-05-20 14:42:21 +0100

    w

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tkinter
+import requests
+import tkinter as tk
+from tkinter import messagebox, scrolledtext, ttk
+import os
+import spoonacular as sp
 
-niggas
+#Primeiro criar a funcionalidade para a pessoa dizer o que quer
+
+
 API_KEY = "102e440d05e14a75b434d6de15670598"
 BASE_URL = "https://api.spoonacular.com"
